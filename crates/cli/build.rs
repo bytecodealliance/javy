10c04dbc27711b6b68e0544d564dfab1b71dcce3
Maxime Bedard <maxime.bedard@shopify.com>
2021-08-09 11:10:40 -0400

    fix build script

--- crates/cli/build.rs
+++ crates/cli/build.rs
@@ -2,16 +2,24 @@ use std::env;
 use std::path::PathBuf;
 
 fn main() {
+    let out_dir: PathBuf = env::var("OUT_DIR")
+        .expect("failed to retrieve out dir")
+        .into();
+
+    // When using clippy, we need to write a stubbed engine.wasm file to ensure compilation succeeds.
+    if let Ok("cargo-clippy") = env::var("CARGO_CFG_FEATURE").as_ref().map(String::as_str) {
+        std::fs::write(out_dir.join("engine.wasm"), "")
+            .expect("failed to write empty engine.wasm stub");
+        std::process::exit(0);
+    }
+
     let profile = env::var("PROFILE").expect("Couldn't retrieve profile");
-    if profile != "release" {
-        eprintln!("only --release is supported due to https://github.com/bytecodealliance/wizer/issues/27");
+    if profile == "debug" {
+        eprintln!("debug profile is not supported due to https://github.com/bytecodealliance/wizer/issues/27");
         std::process::exit(1);
     }
 
-    let out_dir: PathBuf = env::var("OUT_DIR")
-        .expect("Couldn't retrieve out dir")
-        .into();
-    let root: PathBuf = std::env::var("CARGO_MANIFEST_DIR")
+    let engine_path: PathBuf = std::env::var("CARGO_MANIFEST_DIR")
         .map(PathBuf::from)
         .ok()
         .map(|mut b| {
@@ -24,11 +32,11 @@ fn main() {
         })
         .expect("failed to create path");
 
-    if !root.exists() {
-        eprintln!("compile core using `cd crates/core && cargo build && cd -`");
-        std::process::exit(1);
-    }
+    println!("cargo:rerun-if-changed={:?}", engine_path);
 
-    println!("cargo:rerun-if-changed={:?}", root);
-    std::fs::copy(root, out_dir.join("engine.wasm")).unwrap();
+    // Only copy the file when it exists. Cargo will take care of re-running this script when the file changes.
+    if engine_path.exists() {
+        std::fs::copy(&engine_path, out_dir.join("engine.wasm"))
+            .expect(format!("failed to copy engine from {:?}", engine_path).as_str());
+    }
 }
