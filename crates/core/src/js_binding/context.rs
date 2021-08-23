1a316e6f746dc003ea1b313e69049a4db478bc45
Saúl Cabrera <saulecabrera@gmail.com>
2021-08-23 09:06:05 -0400

    feat: Add inner, global_object and call functions
    This commit adds inner, global_object and call functions. All these functions
    will return `Result<Value>`

--- crates/core/src/js_binding/context.rs
+++ crates/core/src/js_binding/context.rs
@@ -27,34 +27,53 @@ impl Context {
         Ok(Self { runtime, inner })
     }
 
-    pub(crate) fn eval_global(&self, name: &str, contents: &[u8]) -> Result<Value> {
-        let input = c_string_from_bytes(contents)?;
+    pub(crate) fn inner(&self) -> *mut JSContext {
+        self.inner
+    }
+
+    pub(crate) fn eval_global(&self, name: &str, contents: &str) -> Result<Value> {
+        let input = CString::new(contents)?;
+        let script_name = CString::new(name)?;
         let len = contents.len() - 1;
         let raw = unsafe {
             JS_Eval(
                 self.inner,
                 input.as_ptr(),
                 len as _,
-                // not sure why this is ok in the previous
-                // impl but here I need to cast to *const i8
-                name.as_ptr() as *const i8,
+                script_name.as_ptr(),
                 JS_EVAL_TYPE_GLOBAL as i32,
             )
         };
 
         Value::new(self, raw)
     }
-}
 
-fn c_string_from_bytes(bytes: impl Into<Vec<u8>>) -> Result<CString> {
-    let cstring = CString::new(bytes)?;
-    Ok(cstring)
+    pub(crate) fn global_object(&self) -> Result<Value> {
+        let raw = unsafe { JS_GetGlobalObject(self.inner) };
+        Value::new(self, raw)
+    }
+
+    pub(crate) fn call(&self, fun: &Value, receiver: &Value, args: &[Value]) -> Result<Value> {
+        let inner_args: Vec<JSValue> = args.iter().map(|v| v.inner()).collect();
+        let return_val = unsafe {
+            JS_Call(
+                self.inner,
+                fun.inner(),
+                receiver.inner(),
+                args.len() as i32,
+                inner_args.as_slice().as_ptr() as *mut JSValue,
+            )
+        };
+
+        Value::new(self, return_val)
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use super::Context;
     use anyhow::Result as R;
+    const SCRIPT_NAME: &str = "context.js";
 
     #[test]
     fn test_new_returns_a_context() -> R<()> {
@@ -66,9 +85,8 @@ mod tests {
     #[test]
     fn test_context_evalutes_code_globally() -> R<()> {
         let ctx = Context::new()?;
-        let name = "script.js";
         let contents = "var a = 1;";
-        let val = ctx.eval_global(&name, contents.as_bytes());
+        let val = ctx.eval_global(SCRIPT_NAME, contents);
         assert!(val.is_ok());
         Ok(())
     }
@@ -76,10 +94,29 @@ mod tests {
     #[test]
     fn test_context_reports_invalid_code() -> R<()> {
         let ctx = Context::new()?;
-        let name = "script.js";
         let contents = "a + 1 * z;";
-        let val = ctx.eval_global(&name, contents.as_bytes());
+        let val = ctx.eval_global(SCRIPT_NAME, contents);
         assert!(val.is_err());
+        Ok(())
+    }
+
+    #[test]
+    fn test_context_allows_access_to_global_object() -> R<()> {
+        let ctx = Context::new()?;
+        let val = ctx.global_object();
+        assert!(val.is_ok());
+        Ok(())
+    }
+
+    #[test]
+    fn test_context_allows_calling_a_function() -> R<()> {
+        let ctx = Context::new()?;
+        let contents = "globalThis.foo = function() { return 1; }";
+        let _ = ctx.eval_global(SCRIPT_NAME, contents)?;
+        let global = ctx.global_object()?;
+        let fun = global.get_property("foo")?;
+        let result = ctx.call(&fun, &global, &[]);
+        assert!(result.is_ok());
         Ok(())
     }
 }
