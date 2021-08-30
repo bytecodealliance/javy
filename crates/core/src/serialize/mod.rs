a5c440e2aaaba1ef74dd06ca9ac2e0d9a4660872
Maxime Bedard <maxime.bedard@shopify.com>
2021-08-30 09:23:42 -0400

    add roundtrip tests for maps

--- crates/core/src/serialize/mod.rs
+++ crates/core/src/serialize/mod.rs
@@ -23,4 +23,37 @@ mod tests {
             Ok(v == result)
         }
     }
+
+    #[test]
+    fn test_map() {
+        let mut expected = BTreeMap::<String, String>::new();
+        expected.insert("foo".to_string(), "bar".to_string());
+        expected.insert("hello".to_string(), "world".to_string());
+
+        let context = Context::default();
+        let mut serializer = ValueSerializer::from_context(context);
+        expected.serialize(&mut serializer).unwrap();
+        let mut deserializer = ValueDeserializer::from(&context, serializer.value);
+        let actual = BTreeMap::<String, String>::deserialize(&mut deserializer).unwrap();
+
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn test_struct_into_map() {
+        #[derive(Debug, Serialize, Deserialize, PartialEq)]
+        struct MyObject {
+            foo: String,
+            bar: u32,
+        }
+        let expected = MyObject { foo: "hello".to_string(), bar: 1337 };
+
+        let context = Context::default();
+        let mut serializer = ValueSerializer::from_context(context);
+        expected.serialize(&mut serializer).unwrap();
+        let mut deserializer = ValueDeserializer::from(&context, serializer.value);
+        let actual = MyObject::deserialize(&mut deserializer).unwrap();
+
+        assert_eq!(expected, actual);
+    }
 }
