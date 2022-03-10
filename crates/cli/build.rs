69757e0fe8d850606d6dde9c1c1ab4e5a6484a44
Jackson Hong <jackson.hong@shopify.com>
2022-03-10 09:36:52 -0500

    Move build.rs back to root dir and move opt.rs code directly into build.rs

--- crates/cli/build.rs
+++ crates/cli/build.rs
@@ -1,8 +1,63 @@
-mod opt;
-
 use std::env;
 use std::fs;
 use std::path::PathBuf;
+
+use anyhow::{bail, Error, Result};
+use binaryen::{CodegenConfig, Module};
+use std::path::Path;
+use wizer::Wizer;
+
+pub(crate) struct Optimizer<'a> {
+    optimize: bool,
+    wasm: &'a [u8],
+}
+
+impl<'a> Optimizer<'a> {
+    pub fn new(wasm: &'a [u8]) -> Self {
+        Self {
+            wasm,
+            optimize: false,
+        }
+    }
+
+    pub fn optimize(self, optimize: bool) -> Self {
+        Self { optimize, ..self }
+    }
+
+    pub fn write_optimized_wasm(
+        self,
+        dest: impl AsRef<Path>,
+        init_func: String,
+    ) -> Result<(), Error> {
+        let mut wasm = Wizer::new()
+            .allow_wasi(true)
+            .inherit_stdio(true)
+            .init_func(init_func)
+            .run(self.wasm)?;
+
+        if self.optimize {
+            let codegen_cfg = CodegenConfig {
+                optimization_level: 3, // Aggressively optimize for speed.
+                shrink_level: 0,       // Don't optimize for size at the expense of performance.
+                debug_info: false,
+            };
+
+            if let Ok(mut module) = Module::read(&wasm) {
+                module.optimize(&codegen_cfg);
+                module
+                    .run_optimization_passes(vec!["strip"], &codegen_cfg)
+                    .unwrap();
+                wasm = module.write();
+            } else {
+                bail!("Unable to read wasm binary for wasm-opt optimizations");
+            }
+        }
+
+        std::fs::write(dest.as_ref(), wasm)?;
+
+        Ok(())
+    }
+}
 
 fn main() {
     if let Ok("cargo-clippy") = env::var("CARGO_CFG_FEATURE").as_ref().map(String::as_str) {
@@ -43,7 +98,7 @@ fn copy_engine_binary() {
     if engine_path.exists() {
         let copied_engine_path = PathBuf::from(env::var("OUT_DIR").unwrap()).join("engine.wasm");
         let engine_wasm = fs::read(&engine_path).expect("failed to read wasm module");
-        opt::Optimizer::new(engine_wasm.as_slice())
+        Optimizer::new(engine_wasm.as_slice())
             .optimize(true)
             .write_optimized_wasm(&copied_engine_path, "init_engine".to_owned())
             .expect("failed to write optimized wasm");
