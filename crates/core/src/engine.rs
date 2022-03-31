5718405d2aab78ae1827faf47d88607876f1df6f
Jimmy Bourassa <jimmy.bourassa@shopify.com>
2022-03-31 10:39:58 -0400

    Add JSON support to quickjs-wasm-rs and CLI
    Co-authored-by: Jeffrey Charles <jeff.charles@shopify.com>

--- crates/core/src/engine.rs
+++ crates/core/src/engine.rs
@@ -5,31 +5,12 @@ pub fn load() -> Result<Vec<u8>> {
     let mut reader = stdin();
     let mut output: Vec<u8> = vec![];
 
-    #[cfg(not(feature = "json-io"))]
-    {
-        copy(&mut reader, &mut output)?;
-        Ok(output)
-    }
-    #[cfg(feature = "json-io")]
-    {
-        let mut input: Vec<u8> = vec![];
-        copy(&mut reader, &mut input)?;
-        let mut deserializer = serde_json::Deserializer::from_slice(&input);
-        let mut serializer = rmp_serde::Serializer::new(&mut output);
-        serde_transcode::transcode(&mut deserializer, &mut serializer)?;
-        Ok(output)
-    }
-}
-pub fn store(bytes: &[u8]) -> Result<()> {
-    #[cfg(feature = "json-io")]
-    let bytes = &{
-        let mut output = Vec::new();
-        let mut deserializer = rmp_serde::Deserializer::from_read_ref(bytes);
-        let mut serializer = serde_json::Serializer::new(&mut output);
-        serde_transcode::transcode(&mut deserializer, &mut serializer)?;
-        output
-    };
+    copy(&mut reader, &mut output)?;
 
+    Ok(output)
+}
+
+pub fn store(bytes: &[u8]) -> Result<()> {
     let mut handle = stdout();
     handle.write_all(bytes)?;
     handle.flush()?;
