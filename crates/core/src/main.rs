1b18c45f7aafe506d68dca076f787a81c17d0b21
Surma <surma@shopify.com>
2022-10-13 11:41:54 +0100

    Minor cleanup

--- crates/core/src/main.rs
+++ crates/core/src/main.rs
@@ -1,6 +1,4 @@
-mod engine;
-
-use quickjs_wasm_rs::{json, Context, Value};
+use quickjs_wasm_rs::{Context, Value};
 
 use once_cell::sync::OnceCell;
 use std::io::{self, Read, Write};
@@ -25,14 +23,6 @@ pub extern "C" fn init() {
         .register_globals(io::stderr(), io::stderr())
         .unwrap();
 
-    context
-        .eval_global(
-            "prelude.js",
-            r#"
-            "#,
-        )
-        .unwrap();
-
     let mut contents = String::new();
     io::stdin().read_to_string(&mut contents).unwrap();
 
@@ -42,41 +32,45 @@ pub extern "C" fn init() {
     }
 }
 
-fn create_wasi_global(context: &Context) -> Value {
+fn create_wasi_object(context: &Context) -> Value {
     let wasi_global = context.object_value().unwrap();
 
     unsafe {
-        wasi_global.set_property(
-            "writeStdout",
-            context
-                .new_callback(|_ctx, _this, argc, argv, _magic| {
-                    if argc != 1 {
-                        return context.exception_value().unwrap().into();
-                    }
-                    let param = unsafe { context.value_from_ptr(*argv.offset(0)).unwrap() };
-                    let str = param.as_str().unwrap();
-                    io::stdout().write_all(str.as_bytes()).unwrap();
-                    context.undefined_value().unwrap().into()
-                })
-                .unwrap(),
-        );
+        wasi_global
+            .set_property(
+                "writeStdout",
+                context
+                    .new_callback(|_ctx, _this, argc, argv, _magic| {
+                        if argc != 1 {
+                            return context.exception_value().unwrap().into();
+                        }
+                        let param = context.value_from_ptr(*argv.offset(0)).unwrap();
+                        let str = param.as_str().unwrap();
+                        io::stdout().write_all(str.as_bytes()).unwrap();
+                        context.undefined_value().unwrap().into()
+                    })
+                    .unwrap(),
+            )
+            .unwrap();
 
-        wasi_global.set_property(
-            "readStdin",
-            context
-                .new_callback(|_ctx, _this, argc, _argv, _magic| {
-                    if argc != 0 {
-                        return context.exception_value().unwrap().into();
-                    }
+        wasi_global
+            .set_property(
+                "readStdin",
+                context
+                    .new_callback(|_ctx, _this, argc, _argv, _magic| {
+                        if argc != 0 {
+                            return context.exception_value().unwrap().into();
+                        }
 
-                    let mut buffer: String = String::new();
-                    io::stdin().read_to_string(&mut buffer).unwrap();
+                        let mut buffer: String = String::new();
+                        io::stdin().read_to_string(&mut buffer).unwrap();
 
-                    let val = context.value_from_str(&buffer).unwrap();
-                    val.into()
-                })
-                .unwrap(),
-        );
+                        let val = context.value_from_str(&buffer).unwrap();
+                        val.into()
+                    })
+                    .unwrap(),
+            )
+            .unwrap();
     }
 
     wasi_global
@@ -87,10 +81,9 @@ fn main() {
     let context = unsafe { CONTEXT.take().unwrap() };
 
     let global = context.global_object().unwrap();
-    global.set_property("WASI", create_wasi_global(&context));
+    global
+        .set_property("WASI", create_wasi_object(&context))
+        .unwrap();
 
     context.eval_global("function.mjs", &code).unwrap();
-
-    // let output = json::transcode_output(output_value.unwrap()).unwrap();
-    // engine::store(&output).expect("Couldn't store output");
 }
