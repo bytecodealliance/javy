bcdea89b252b38376ca4e230550fb6f5b367fd18
Surma <surma@shopify.com>
2022-11-30 13:22:30 +0000

    Fix it up

--- wpt/rollup.config.js
+++ wpt/rollup.config.js
@@ -12,6 +12,29 @@ export default {
     format: "es",
   },
   plugins: [
+    // This plugin transforms the WPT-invented `// META: script=`
+    // directives into ESM imports.
+    {
+      name: "wpt-import-sytnax",
+      transform(chunk, id) {
+        const imports = [];
+        const newCode = chunk.replaceAll(MATCHER, (_match, ref) => {
+          let base;
+          if (ref.startsWith(".")) {
+            base = path.dirname(id);
+          } else {
+            base = projectRoot;
+          }
+          const refPath = path.join(base, ref);
+          imports.push(refPath);
+          return "";
+        });
+        return `
+          ${imports.map((i) => `import ${JSON.stringify(i)};`).join("\n")}
+          ${newCode}
+        `;
+      },
+    },
     // This plugin injects an import for the test harness
     // into the top-level file and fixes up the global scope with
     // stuff we need.
@@ -31,35 +54,30 @@ export default {
         if (id !== "custom:globalFix") return;
         return `
 					globalThis.self = globalThis;
-					global.location = {};
+					globalThis.location = {};
 				`;
       },
       transform(chunk, id) {
         if (!this[PRIVATE].has(id)) return;
+        // OMG this is super brittle, but should work for now.
+        const lines = chunk.split("\n");
+        const importLines = lines.filter((line) =>
+          line.trim().startsWith("import")
+        );
+        const otherLines = lines.filter(
+          (line) => !line.trim().startsWith("import")
+        );
         return `
 					import "custom:globalFix";
 					import "${path.join(projectRoot, "/resources/testharness.js")}";
 					import reporter from "${path.join(projectRoot, "/../reporter.js")}";
-					add_completion_callback(reporter);
-					${chunk}
-				`;
-      },
-    },
-    // This plugin transforms the WPT-invented `// META: script=`
-    // directives into ESM imports.
-    {
-      name: "wpt-import-sytnax",
-      transform(chunk, id) {
-        return chunk.replaceAll(MATCHER, (_match, ref) => {
-          let base;
-          if (ref.startsWith(".")) {
-            base = path.dirname(id);
-          } else {
-            base = projectRoot;
+          ${importLines.join("\n")}
+          function main() {
+  					add_completion_callback(reporter);
+  					${otherLines.join("\n")}
           }
-          const refPath = path.join(base, ref);
-          return `import ${JSON.stringify(refPath)};`;
-        });
+          Shopify = {main};
+				`;
       },
     },
   ],
