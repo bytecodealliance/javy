8efec850bd691a7d6c5787293288bda654fd56c0
Surma <surma@shopify.com>
2022-12-05 13:02:08 +0000

    Add support for regexp in ignoredTests

--- wpt/reporter.js
+++ wpt/reporter.js
@@ -1,8 +1,17 @@
 export function result_reporter(test) {
   // No logging on success;
   if (test.status == 0) return;
-  const ignoredTests = globalThis.ignoredTests ?? [];
-  if (ignoredTests.some((testName) => testName == test.name)) return;
+  let ignoredTests = (globalThis.ignoredTests ?? []).map((matcher) => {
+    if (matcher.startsWith("/")) {
+      return new RegExp(matcher.slice(1).slice(0, -1));
+    }
+    return matcher;
+  });
+  const shouldSkipTest = ignoredTests.some((matcher) => {
+    if (matcher instanceof RegExp) return matcher.test(test.name);
+    return matcher == test.name;
+  });
+  if (shouldSkipTest) return;
   console.log("[FAIL]", test.name);
   console.log(test.message);
   console.log(test.stack);
--- wpt/test_spec.js
+++ wpt/test_spec.js
@@ -1,10 +1,13 @@
 export default [
   {
-    testFile: "./custom_test.js",
-    ignoredTests: ["This is an ignored test"],
+    "testFile": "./custom_test.js",
+    "ignoredTests": ["This is an ignored test"]
   },
   {
-    testFile: "upstream/encoding/textdecoder-fatal-single-byte.any.js",
-    ignoredTests: [],
+    "testFile": "upstream/encoding/textdecoder-eof.any.js",
+    "ignoredTests": ["/stream: true/"]
   },
-];
+  {
+    "testFile": "upstream/encoding/textencoder-utf16-surrogates.any.js",
+  }
+]
