44be687868896c16c53daca24710c5ab68c5d23d
Jeffrey Charles <jeff.charles@shopify.com>
2023-01-23 14:15:43 -0500

    Update crates/quickjs-wasm-rs/src/js_binding/error.rs
    Co-authored-by: Surma <surma@surma.dev>

--- crates/quickjs-wasm-rs/src/js_binding/error.rs
+++ crates/quickjs-wasm-rs/src/js_binding/error.rs
@@ -13,11 +13,12 @@ pub enum JSError {
 impl Display for JSError {
     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
         match self {
-            Self::Internal(msg) => write!(f, "{}", msg),
-            Self::Range(msg) => write!(f, "{}", msg),
-            Self::Reference(msg) => write!(f, "{}", msg),
-            Self::Syntax(msg) => write!(f, "{}", msg),
-            Self::Type(msg) => write!(f, "{}", msg),
+            Self::Internal(msg)
+            | Self::Range(msg)
+            | Self::Reference(msg)
+            | Self::Syntax(msg)
+            | Self::Type(msg) => write!(f, "{}", msg),
+
         }
     }
 }
