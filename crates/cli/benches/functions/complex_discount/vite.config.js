d61efe10d824292a35eabda04c61f79bc87968e4
Jeffrey Charles <jeff.charles@shopify.com>
2023-02-23 10:13:02 -0500

    Include source code for complex_discount (#246)

---
+++ crates/cli/benches/functions/complex_discount/vite.config.js
@@ -0,0 +1,10 @@
+export default {
+    build: {
+        target: "es2015",
+        lib: {
+            entry: "./index.js",
+            formats: ["es"],
+            fileName: "function",
+        },
+    },
+};
