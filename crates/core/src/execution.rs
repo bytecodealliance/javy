3945fad6db6d6cb37eb3f93cc33a0550e73959a0
Jeff Charles <jeff.charles@shopify.com>
2023-03-15 17:34:12 -0400

    Remove WPT test env var

--- crates/core/src/execution.rs
+++ crates/core/src/execution.rs
@@ -1,5 +1,3 @@
-use std::env;
-
 use anyhow::{bail, Result};
 use quickjs_wasm_rs::Context;
 
@@ -7,13 +5,8 @@ pub fn run_bytecode(context: &Context, bytecode: &[u8]) -> Result<()> {
     context.eval_binary(bytecode)?;
     if cfg!(feature = "experimental_event_loop") {
         context.execute_pending()?;
-    } else if context.is_pending() && !is_running_wpt_suite() {
-        // WPT enqueues a promise as part of its suite setup and we can't change that behaviour so don't error
+    } else if context.is_pending() {
         bail!("Adding tasks to the event queue is not supported");
     }
     Ok(())
-}
-
-fn is_running_wpt_suite() -> bool {
-    env::var("JAVY_WPT").is_ok()
 }
