a2a328a32d8c3e33b763de02fac7d9d753cac595
Surma <surma@surma.dev>
2023-05-27 10:07:49 +0100

    Automatically download WASI SDK

--- crates/quickjs-wasm-sys/build.rs
+++ crates/quickjs-wasm-sys/build.rs
@@ -1,21 +1,74 @@
-use std::env;
+use anyhow::{anyhow, Result};
 use std::path::PathBuf;
+use std::{env, fs, process};
 
 use walkdir::WalkDir;
 
-fn main() {
-    let this_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
-    let wasi_sdk_path =
-        env::var("QUICKJS_WASM_SYS_WASI_SDK_PATH").unwrap_or(format!("{this_dir}/wasi-sdk"));
-    if !std::path::Path::exists(std::path::Path::new(&wasi_sdk_path)) {
-        panic!(
-            "wasi-sdk not installed in specified path of {}",
-            &wasi_sdk_path
-        );
+fn download_wasi_sdk() -> Result<PathBuf> {
+    let mut wasi_sdk_dir: PathBuf = env::var("CARGO_MANIFEST_DIR")?.into();
+    wasi_sdk_dir.push("wasi-sdk");
+
+    fs::create_dir_all(&wasi_sdk_dir)?;
+
+    let mut archive_path = wasi_sdk_dir.clone();
+    archive_path.push("wasi-sdk.tar.gz");
+
+    // Download archive if necessary
+    if !archive_path.try_exists()? {
+        let file_suffix = match (env::consts::OS, env::consts::ARCH) {
+            ("linux", "x86") | ("linux", "x86_64") => "linux",
+            ("macos", "x86") | ("macos", "x86_64") | ("macos", "aarch64") => "macos",
+            ("windows", "x86") => "mingw-x86",
+            ("windows", "x86_64") => "mingw",
+            other => return Err(anyhow!("Unsupported platform tuple {:?}", other)),
+        };
+
+        let mut archive = fs::File::create(&archive_path)?;
+        reqwest::blocking::get(&format!("https://github.com/WebAssembly/wasi-sdk/releases/download/wasi-sdk-19/wasi-sdk-19.0-{}.tar.gz", file_suffix))?.copy_to(&mut archive)?;
     }
-    env::set_var("CC", format!("{}/bin/clang", &wasi_sdk_path));
-    env::set_var("AR", format!("{}/bin/ar", &wasi_sdk_path));
-    let sysroot = format!("--sysroot={}/share/wasi-sysroot", &wasi_sdk_path);
+
+    let mut test_binary = wasi_sdk_dir.clone();
+    test_binary.extend(["bin", "wasm-ld"]);
+    // Extract archive if necessary
+    if !test_binary.try_exists()? {
+        let output = process::Command::new("tar")
+            .args([
+                "-xf",
+                archive_path.to_string_lossy().as_ref(),
+                "--strip-components",
+                "1",
+            ])
+            .current_dir(&wasi_sdk_dir)
+            .output()?;
+        if !output.status.success() {
+            return Err(anyhow!(
+                "Unpacking WASI SDK failed: {}",
+                String::from_utf8_lossy(&output.stderr)
+            ));
+        }
+    }
+
+    Ok(wasi_sdk_dir)
+}
+
+fn get_wasi_sdk_path() -> Result<PathBuf> {
+    if let Ok(path) = env::var("QUICKJS_WASM_SYS_WASI_SDK_PATH") {
+        return Ok(path.into());
+    }
+    download_wasi_sdk()
+}
+
+fn main() -> Result<()> {
+    let wasi_sdk_path = get_wasi_sdk_path()?;
+    if !wasi_sdk_path.try_exists()? {
+        return Err(anyhow!(
+            "wasi-sdk not installed in specified path of {}",
+            wasi_sdk_path.display()
+        ));
+    }
+    env::set_var("CC", format!("{}/bin/clang", wasi_sdk_path.display()));
+    env::set_var("AR", format!("{}/bin/ar", wasi_sdk_path.display()));
+    let sysroot = format!("--sysroot={}/share/wasi-sysroot", wasi_sdk_path.display());
     env::set_var("CFLAGS", &sysroot);
 
     // Build quickjs as a static library.
@@ -58,19 +111,16 @@ fn main() {
         .header("wrapper.h")
         .parse_callbacks(Box::new(bindgen::CargoCallbacks))
         .clang_args(&["-fvisibility=default", "--target=wasm32-wasi", &sysroot])
-        .generate()
-        .unwrap();
+        .generate()?;
 
     println!("cargo:rerun-if-changed=extensions/value.c");
     println!("cargo:rerun-if-changed=wrapper.h");
 
     for entry in WalkDir::new("quickjs") {
-        println!(
-            "cargo:rerun-if-changed={}",
-            entry.unwrap().path().to_str().unwrap()
-        );
+        println!("cargo:rerun-if-changed={}", entry?.path().display());
     }
 
-    let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
-    bindings.write_to_file(out_dir.join("bindings.rs")).unwrap();
+    let out_dir = PathBuf::from(env::var("OUT_DIR")?);
+    bindings.write_to_file(out_dir.join("bindings.rs"))?;
+    Ok(())
 }
