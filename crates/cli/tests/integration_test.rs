39a3e70a80af07528cdab4d56e60d55515acc0d2
Jeff Charles <jeff.charles@shopify.com>
2023-06-16 17:15:38 -0400

    Remove flaky fuel assert for random test case

--- crates/cli/tests/integration_test.rs
+++ crates/cli/tests/integration_test.rs
@@ -129,11 +129,11 @@ fn test_error_handling() {
 #[test]
 fn test_same_module_outputs_different_random_result() {
     let mut runner = Runner::new("random.js");
-    let (output, _, fuel_consumed) = runner.exec(&[]).unwrap();
+    let (output, _, _) = runner.exec(&[]).unwrap();
     let (output2, _, _) = runner.exec(&[]).unwrap();
     // In theory these could be equal with a correct implementation but it's very unlikely.
     assert!(output != output2);
-    assert_fuel_consumed_within_threshold(100_543, fuel_consumed);
+    // Don't check fuel consumed because fuel consumed can be different from run to run.
 }
 
 fn run_with_u8s(r: &mut Runner, stdin: u8) -> (u8, String, u64) {
