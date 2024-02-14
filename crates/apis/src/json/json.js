5dab682010c3ca160ca0e840fe46d7dea6e4a453
Saúl Cabrera <saulecabrera@gmail.com>
2024-02-14 14:01:38 -0500

    Fix .js definition

--- crates/apis/src/json/json.js
+++ crates/apis/src/json/json.js
@@ -1,12 +1,10 @@
 (function () {
-  const __javy_io_readSync = globalThis.__javy_io_readSync;
-  const __javy_io_writeSync = globalThis.__javy_io_writeSync;
+  const __javy_json_parse = globalThis.__javy_json_parse;
   globalThis.Javy.JSON = {
     parseFromStdin(fd, data) {
       return __javy_json_parse();
     },
   };
 
-  Reflect.deleteProperty(globalThis, "__javy_io_readSync");
-  Reflect.deleteProperty(globalThis, "__javy_io_writeSync");
+  Reflect.deleteProperty(globalThis, "__javy_json_parse");
 })();
