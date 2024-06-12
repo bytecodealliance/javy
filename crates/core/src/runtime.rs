f680f1fa9ef6f5938ce081c954ecaa05a7322b8e
Saúl Cabrera <saulecabrera@gmail.com>
2024-06-12 09:44:05 -0400

    Introduce the `javy-config` crate (#664)
    * Introduce the `javy-config` crate
    
    This commit prepares the terrain to bridge the gap in configurability
    between the CLI and Javy, the crate.
    The intention behind introducing a new crate is to:
    
    * Reduce code duplication and sync the options between the CLI and the
      core crate.
    * Make it easy to pass the options into WebAssemlby by using bitflags.
    
    This PR doesn't introduce any new functionality. A follow up PR will
    include new commands in the CLI which will make use of the share
    configuration.
    
    * Add tests
    
    * Fix typo

--- crates/core/src/runtime.rs
+++ crates/core/src/runtime.rs
@@ -1,14 +1,17 @@
 use anyhow::Result;
 use javy::{Config, Runtime};
+use javy_config::Config as SharedConfig;
 
-pub(crate) fn new_runtime() -> Result<Runtime> {
+pub(crate) fn new(shared_config: SharedConfig) -> Result<Runtime> {
     let mut config = Config::default();
     let config = config
-        .text_encoding(true)
-        .redirect_stdout_to_stderr(true)
-        .javy_stream_io(true)
-        .override_json_parse_and_stringify(true)
-        .javy_json(true);
+        .text_encoding(shared_config.contains(SharedConfig::TEXT_ENCODING))
+        .redirect_stdout_to_stderr(shared_config.contains(SharedConfig::REDIRECT_STDOUT_TO_STDERR))
+        .javy_stream_io(shared_config.contains(SharedConfig::JAVY_STREAM_IO))
+        .override_json_parse_and_stringify(
+            shared_config.contains(SharedConfig::OVERRIDE_JSON_PARSE_AND_STRINGIFY),
+        )
+        .javy_json(shared_config.contains(SharedConfig::JAVY_JSON));
 
     Runtime::new(std::mem::take(config))
 }
---
+++ crates/javy-config/src/lib.rs
@@ -0,0 +1,48 @@
+//! Shared Configuration for Javy.
+//!
+//! This crate serves as a central place to facilitate configuration sharing
+//! between the Javy CLI and the Javy crate. It addresses the challenge of
+//! passing configuration settings in environments where the Javy CLI commands
+//! predominantly execute WebAssembly.
+//!
+//! The purpose of this crate is to consolidate configuration parameters,
+//! ensuring consistent and accessible settings across both the CLI and the
+//! crate. This approach simplifies the management of configuration settings and
+//! enhances the integration between different components of the Javy ecosystem.
+//!
+//! Currently, this crate includes only a subset of the available configuration
+//! options. The objective is to eventually encompass all configurable
+//! parameters found in [javy::Config].
+//!
+//! The selection of the current configuration options was influenced by the
+//! need to override non-standard defaults typically set during CLI invocations.
+//! These defaults often do not align with the preferences of the CLI users.
+//!
+//! In general this crate should be treated as an internal detail and
+//! a contract between the CLI and the Javy crate.
+
+use bitflags::bitflags;
+
+bitflags! {
+    #[derive(Eq, PartialEq)]
+    pub struct Config: u32 {
+        const OVERRIDE_JSON_PARSE_AND_STRINGIFY = 1;
+        const JAVY_JSON = 1 << 1;
+        const JAVY_STREAM_IO = 1 << 2;
+        const REDIRECT_STDOUT_TO_STDERR = 1 << 3;
+        const TEXT_ENCODING = 1 << 4;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Config;
+    #[test]
+    fn check_bits() {
+        assert!(Config::OVERRIDE_JSON_PARSE_AND_STRINGIFY == Config::from_bits(1).unwrap());
+        assert!(Config::JAVY_JSON == Config::from_bits(1 << 1).unwrap());
+        assert!(Config::JAVY_STREAM_IO == Config::from_bits(1 << 2).unwrap());
+        assert!(Config::REDIRECT_STDOUT_TO_STDERR == Config::from_bits(1 << 3).unwrap());
+        assert!(Config::TEXT_ENCODING == Config::from_bits(1 << 4).unwrap());
+    }
+}
