fc0387b71339fbd264612095cbfa916e58ffdce0
Jeffrey Charles <jeff.charles@shopify.com>
2025-08-27 10:13:13 -0400

    Add snapshot tests for dynamically linked modules (#989)

---
+++ crates/codegen/tests/sample-scripts/exported-functions.js
@@ -0,0 +1,9 @@
+export function log() {
+    console.log("Hello from function!");
+}
+
+export function log2() {
+    console.log("Hello from function2!");
+}
+
+console.log("Hello from top-level scope");
